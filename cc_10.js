36680dbe0ae5b2790258b57d4dfe2d82e55e7e84
Oliviagiles09 <giles11@usf.edu>
2025-02-23 22:53:06 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -20,3 +20,20 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails());
+
+// Task 2 
+class Order {
+  constructor(orderID, product, quantity) {
+    this.orderId = orderId;
+    this.product = product;
+    this.quantity = quantity;
+    this.product.updateStock(this.quantity);
+  };
+
+getOrderDetails() {
+  return 'Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}"';
+};
+};
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+console.log(prod1.getDetails());
